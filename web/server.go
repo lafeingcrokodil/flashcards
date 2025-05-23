7bc34e621854b70d29a48764dd49abcaf188386b
lafeingcrokodil <tvhtoo@gmail.com>
2025-05-24 00:03:02 +0200

    Add support for submitting answers

--- web/server.go
+++ web/server.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/lafeingcrokodil/flashcards/review"
 )
@@ -18,19 +19,71 @@ type Server struct {
 
 // Start starts the server.
 func (s *Server) Start(addr string) error {
-	http.HandleFunc("/session", s.session)
+	http.HandleFunc("/state", s.state)
+	http.HandleFunc("/submit", s.submit)
 	http.Handle("/", http.FileServer(http.Dir("./public")))
 	return http.ListenAndServe(addr, nil)
 }
 
-func (s *Server) session(w http.ResponseWriter, _ *http.Request) {
+func (s *Server) state(w http.ResponseWriter, _ *http.Request) {
 	b, err := json.MarshalIndent(s.Session, "", "  ")
 	if err != nil {
-		fmt.Printf("ERROR: %v", err)
+		fmt.Printf("ERROR: %v\n", err)
+		_, err := fmt.Fprint(w, "")
+		if err != nil {
+			fmt.Printf("ERROR: %v\n", err)
+		}
+		return
 	}
 
 	_, err = fmt.Fprint(w, string(b))
 	if err != nil {
-		fmt.Printf("ERROR: %v", err)
+		fmt.Printf("ERROR: %v\n", err)
+	}
+}
+
+func (s *Server) submit(w http.ResponseWriter, req *http.Request) {
+	query := req.URL.Query()
+	answer := query.Get("answer")
+	isFirstGuess, err := stringToBool(query.Get("isFirstGuess"))
+	if err != nil {
+		fmt.Printf("ERROR: %v\n", err)
+		_, err := fmt.Fprint(w, "")
+		if err != nil {
+			fmt.Printf("ERROR: %v\n", err)
+		}
+		return
+	}
+
+	ok := s.Session.Submit(answer, isFirstGuess)
+	if ok {
+		err := s.saveToFile()
+		if err != nil {
+			fmt.Printf("ERROR: %v\n", err)
+		}
+	}
+
+	_, err = fmt.Fprintf(w, "%v", ok)
+	if err != nil {
+		fmt.Printf("ERROR: %v\n", err)
+	}
+}
+
+func (s *Server) saveToFile() error {
+	b, err := json.MarshalIndent(s.Session, "", "  ")
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(s.BackupPath, b, 0600)
+}
+
+func stringToBool(s string) (bool, error) {
+	switch s {
+	case "true":
+		return true, nil
+	case "false":
+		return false, nil
+	default:
+		return false, fmt.Errorf("invalid boolean string: %s", s)
 	}
 }
