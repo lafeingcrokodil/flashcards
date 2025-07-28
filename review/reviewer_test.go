d1013b2d30d895ab206ab77521173d054c3554de
lafeingcrokodil <tvhtoo@gmail.com>
2025-07-28 11:11:53 +0200

    Add test case for incorrect guess

--- review/reviewer_test.go
+++ review/reviewer_test.go
@@ -7,129 +7,120 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestReviewer_Next(t *testing.T) {
+func TestReviewer_NextFlashcard(t *testing.T) {
+	numFlashcards := 3
+	numProficiencyLevels := 3
+
 	expectedInitialSession := &SessionMetadata{
 		IsNewRound:        true,
-		ProficiencyCounts: make([]int, 5),
-		UnreviewedCount:   3,
+		ProficiencyCounts: make([]int, numProficiencyLevels),
+		UnreviewedCount:   numFlashcards,
 	}
 
-	expectedStates := []struct {
-		session   *SessionMetadata
-		flashcard *Flashcard
+	testCases := []struct {
+		correct           bool
+		isFirstGuess      bool
+		expectedFlashcard *Flashcard
+		expectedSession   *SessionMetadata
 	}{
 		{
-			flashcard: &Flashcard{
+			correct:      true,
+			isFirstGuess: true,
+			expectedFlashcard: &Flashcard{
 				Metadata: flashcardMetadata(1),
 			},
-			session: &SessionMetadata{Round: 0, IsNewRound: false, ProficiencyCounts: []int{0, 1, 0, 0, 0}, UnreviewedCount: 2},
+			expectedSession: &SessionMetadata{Round: 0, IsNewRound: false, ProficiencyCounts: []int{0, 1, 0}, UnreviewedCount: 2},
 		},
 		{
-			flashcard: &Flashcard{
+			correct:      true,
+			isFirstGuess: true,
+			expectedFlashcard: &Flashcard{
 				Metadata: flashcardMetadata(2),
 			},
-			session: &SessionMetadata{Round: 1, IsNewRound: false, ProficiencyCounts: []int{0, 2, 0, 0, 0}, UnreviewedCount: 1},
+			expectedSession: &SessionMetadata{Round: 1, IsNewRound: false, ProficiencyCounts: []int{0, 2, 0}, UnreviewedCount: 1},
 		},
 		{
-			flashcard: &Flashcard{
+			correct:      true,
+			isFirstGuess: true,
+			expectedFlashcard: &Flashcard{
 				Metadata: flashcardMetadata(1),
 				Stats:    FlashcardStats{ViewCount: 1, Repetitions: 1, NextReview: 1},
 			},
-			session: &SessionMetadata{Round: 1, IsNewRound: false, ProficiencyCounts: []int{0, 1, 1, 0, 0}, UnreviewedCount: 1},
+			expectedSession: &SessionMetadata{Round: 1, IsNewRound: false, ProficiencyCounts: []int{0, 1, 1}, UnreviewedCount: 1},
 		},
 		{
-			flashcard: &Flashcard{
+			correct:      true,
+			isFirstGuess: true,
+			expectedFlashcard: &Flashcard{
 				Metadata: flashcardMetadata(3),
 			},
-			session: &SessionMetadata{Round: 2, IsNewRound: false, ProficiencyCounts: []int{0, 2, 1, 0, 0}, UnreviewedCount: 0},
+			expectedSession: &SessionMetadata{Round: 2, IsNewRound: false, ProficiencyCounts: []int{0, 2, 1}, UnreviewedCount: 0},
 		},
 		{
-			flashcard: &Flashcard{
+			correct:      true,
+			isFirstGuess: true,
+			expectedFlashcard: &Flashcard{
 				Metadata: flashcardMetadata(2),
 				Stats:    FlashcardStats{ViewCount: 1, Repetitions: 1, NextReview: 2},
 			},
-			session: &SessionMetadata{Round: 2, IsNewRound: false, ProficiencyCounts: []int{0, 1, 2, 0, 0}, UnreviewedCount: 0},
+			expectedSession: &SessionMetadata{Round: 2, IsNewRound: false, ProficiencyCounts: []int{0, 1, 2}, UnreviewedCount: 0},
 		},
 		{
-			flashcard: &Flashcard{
+			correct:      true,
+			isFirstGuess: true,
+			expectedFlashcard: &Flashcard{
 				Metadata: flashcardMetadata(1),
 				Stats:    FlashcardStats{ViewCount: 2, Repetitions: 2, NextReview: 3},
 			},
-			session: &SessionMetadata{Round: 3, IsNewRound: false, ProficiencyCounts: []int{0, 1, 1, 1, 0}, UnreviewedCount: 0},
+			expectedSession: &SessionMetadata{Round: 3, IsNewRound: false, ProficiencyCounts: []int{0, 1, 2}, UnreviewedCount: 0},
 		},
 		{
-			flashcard: &Flashcard{
+			correct:      false,
+			isFirstGuess: true,
+			expectedFlashcard: &Flashcard{
 				Metadata: flashcardMetadata(3),
 				Stats:    FlashcardStats{ViewCount: 1, Repetitions: 1, NextReview: 3},
 			},
-			session: &SessionMetadata{Round: 3, IsNewRound: false, ProficiencyCounts: []int{0, 0, 2, 1, 0}, UnreviewedCount: 0},
+			expectedSession: &SessionMetadata{Round: 3, IsNewRound: false, ProficiencyCounts: []int{0, 1, 2}, UnreviewedCount: 0},
 		},
 		{
-			flashcard: &Flashcard{
-				Metadata: flashcardMetadata(2),
-				Stats:    FlashcardStats{ViewCount: 2, Repetitions: 2, NextReview: 4},
-			},
-			session: &SessionMetadata{Round: 4, IsNewRound: false, ProficiencyCounts: []int{0, 0, 1, 2, 0}, UnreviewedCount: 0},
-		},
-		{
-			flashcard: &Flashcard{
+			correct:      true,
+			isFirstGuess: false,
+			expectedFlashcard: &Flashcard{
 				Metadata: flashcardMetadata(3),
-				Stats:    FlashcardStats{ViewCount: 2, Repetitions: 2, NextReview: 5},
+				Stats:    FlashcardStats{ViewCount: 1, Repetitions: 1, NextReview: 3},
 			},
-			session: &SessionMetadata{Round: 5, IsNewRound: false, ProficiencyCounts: []int{0, 0, 0, 3, 0}, UnreviewedCount: 0},
-		},
-		// No cards to review in round 6.
-		{
-			flashcard: &Flashcard{
-				Metadata: flashcardMetadata(1),
-				Stats:    FlashcardStats{ViewCount: 3, Repetitions: 3, NextReview: 7},
-			},
-			session: &SessionMetadata{Round: 7, IsNewRound: false, ProficiencyCounts: []int{0, 0, 0, 2, 1}, UnreviewedCount: 0},
-		},
-		{
-			flashcard: &Flashcard{
-				Metadata: flashcardMetadata(2),
-				Stats:    FlashcardStats{ViewCount: 3, Repetitions: 3, NextReview: 8},
-			},
-			session: &SessionMetadata{Round: 8, IsNewRound: false, ProficiencyCounts: []int{0, 0, 0, 1, 2}, UnreviewedCount: 0},
-		},
-		{
-			flashcard: &Flashcard{
-				Metadata: flashcardMetadata(3),
-				Stats:    FlashcardStats{ViewCount: 3, Repetitions: 3, NextReview: 9},
-			},
-			session: &SessionMetadata{Round: 9, IsNewRound: false, ProficiencyCounts: []int{0, 0, 0, 0, 3}, UnreviewedCount: 0},
-		},
-		// No cards to review in rounds 10-14.
-		{
-			flashcard: &Flashcard{
-				Metadata: flashcardMetadata(1),
-				Stats:    FlashcardStats{ViewCount: 4, Repetitions: 4, NextReview: 15},
-			},
-			session: &SessionMetadata{Round: 15, IsNewRound: false, ProficiencyCounts: []int{0, 0, 0, 0, 3}, UnreviewedCount: 0},
+			expectedSession: &SessionMetadata{Round: 3, IsNewRound: false, ProficiencyCounts: []int{1, 0, 2}, UnreviewedCount: 0},
 		},
 	}
 
 	ctx := context.Background()
 
-	r := &Reviewer{source: newMemorySource(3), store: NewMemoryStore()}
+	r := &Reviewer{source: newMemorySource(numFlashcards), store: NewMemoryStore()}
 
-	session, err := r.CreateSession(ctx, 5)
+	session, err := r.CreateSession(ctx, numProficiencyLevels)
 	require.NoError(t, err)
 	expectedInitialSession.ID = session.ID
 	require.Equal(t, expectedInitialSession, session)
 
-	for i, expected := range expectedStates {
-		expected.session.ID = expectedInitialSession.ID
+	for i, tc := range testCases {
+		tc.expectedSession.ID = expectedInitialSession.ID
 
 		f, err := r.NextFlashcard(ctx, session.ID)
 		require.NoError(t, err, i)
-		require.Equal(t, expected.flashcard, f, i)
+		require.Equal(t, tc.expectedFlashcard, f, i)
 
-		session, ok, err := r.Submit(ctx, session.ID, f.Metadata.ID, &Submission{Answer: f.Metadata.Answer, IsFirstGuess: true})
+		var answer string
+		if tc.correct {
+			answer = f.Metadata.Answer
+		}
+
+		submission := &Submission{Answer: answer, IsFirstGuess: tc.isFirstGuess}
+
+		session, ok, err := r.Submit(ctx, session.ID, f.Metadata.ID, submission)
 		require.NoError(t, err, i)
-		require.True(t, ok, i)
-		require.Equal(t, expected.session, session, i)
+		require.Equal(t, tc.correct, ok, i)
+		require.Equal(t, tc.expectedSession, session, i)
 	}
 }
 
