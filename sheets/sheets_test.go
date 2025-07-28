5dc173aa6c331f5596c67f1abee9134fbae2a150
lafeingcrokodil <tvhtoo@gmail.com>
2025-07-28 09:20:59 +0200

    Increase test coverage for sheets package

--- sheets/sheets_test.go
+++ sheets/sheets_test.go
@@ -5,23 +5,48 @@ import (
 	"os"
 	"testing"
 
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/assert"
 )
 
 func Test_SheetStore_GetAll(t *testing.T) {
-	expectedRecords := []map[string]string{
-		{"id": "1", "prompt": "P1", "context": "C1", "answer": "A1"},
-		{"id": "2", "prompt": "P1", "context": "C2", "answer": "A2"},
-		{"id": "3", "prompt": "P1", "answer": "A3"},
-		{"id": "4", "prompt": "P2", "context": "C1", "answer": "A1"},
+	spreadsheetID := os.Getenv("FLASHCARDS_SHEETS_ID")
+	cellRange := os.Getenv("FLASHCARDS_SHEETS_CELL_RANGE")
+
+	testCases := []struct {
+		id              string
+		spreadsheetID   string
+		cellRange       string
+		expectedRecords []map[string]string
+		expectedErr     string
+	}{
+		{
+			id:            "Existing cell range",
+			spreadsheetID: spreadsheetID,
+			cellRange:     cellRange,
+			expectedRecords: []map[string]string{
+				{"id": "1", "prompt": "P1", "context": "C1", "answer": "A1"},
+				{"id": "2", "prompt": "P1", "context": "C2", "answer": "A2"},
+				{"id": "3", "prompt": "P1", "answer": "A3"},
+				{"id": "4", "prompt": "P2", "context": "C1", "answer": "A1"},
+			},
+		},
+		{
+			id:            "Nonexistent cell range",
+			spreadsheetID: spreadsheetID,
+			cellRange:     "Nonexistent!A:D",
+			expectedErr:   "googleapi: Error 400: Unable to parse range: Nonexistent!A:D, badRequest",
+		},
 	}
 
 	ctx := context.Background()
 
-	spreadsheetID := os.Getenv("FLASHCARDS_SHEETS_ID")
-	cellRange := os.Getenv("FLASHCARDS_SHEETS_CELL_RANGE")
-
-	records, err := ReadSheet(ctx, spreadsheetID, cellRange)
-	require.NoError(t, err)
-	require.Equal(t, expectedRecords, records)
+	for _, tc := range testCases {
+		records, err := ReadSheet(ctx, tc.spreadsheetID, tc.cellRange)
+		// We use assert instead of require so that we can gather errors for all test cases.
+		if tc.expectedErr != "" {
+			assert.EqualError(t, err, tc.expectedErr, tc.id)
+		} else if !assert.NoError(t, err, tc.id) { //nolint:testifylint
+			assert.Equal(t, tc.expectedRecords, records, tc.id)
+		}
+	}
 }
