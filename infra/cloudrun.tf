c9dc37e57c5343cccaa487d79d63d1197f8b3c48
lafeingcrokodil <tvhtoo@gmail.com>
2025-08-01 20:54:54 +0200

    Deploy via GitHub Actions instead of Terraform

--- infra/cloudrun.tf
+++ infra/cloudrun.tf
@@ -1,17 +1,6 @@
-resource "google_cloud_run_v2_service" "staging" {
+resource "google_cloud_run_v2_service_iam_member" "staging_invoker" {
   name     = "staging"
   location = var.region
-
-  template {
-    containers {
-      image = var.image_url
-    }
-  }
-}
-
-resource "google_cloud_run_v2_service_iam_member" "staging_invoker" {
-  name     = google_cloud_run_v2_service.staging.name
-  location = google_cloud_run_v2_service.staging.location
   role     = "roles/run.invoker"
   for_each = toset(var.staging_invokers)
   member   = each.value
