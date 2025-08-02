80b333eb05e615cd4bd4e8d53e44ccbfcee92bd1
lafeingcrokodil <tvhtoo@gmail.com>
2025-08-02 11:56:34 +0200

    Add Cloud Run domain mappings

--- infra/production.tf
+++ infra/production.tf
@@ -16,3 +16,14 @@ resource "google_service_account_iam_member" "production_users" {
   role               = "roles/iam.serviceAccountUser"
   member             = google_service_account.github_actions.member
 }
+
+resource "google_cloud_run_domain_mapping" "production" {
+  location = var.region
+  name     = "flashcards.lafeingcrokodil.com"
+  metadata {
+    namespace = var.project_id
+  }
+  spec {
+    route_name = "production"
+  }
+}
--- infra/staging.tf
+++ infra/staging.tf
@@ -16,3 +16,14 @@ resource "google_service_account_iam_member" "staging_users" {
   role               = "roles/iam.serviceAccountUser"
   member             = google_service_account.github_actions.member
 }
+
+resource "google_cloud_run_domain_mapping" "staging" {
+  location = var.region
+  name     = "staging.flashcards.lafeingcrokodil.com"
+  metadata {
+    namespace = var.project_id
+  }
+  spec {
+    route_name = "staging"
+  }
+}
