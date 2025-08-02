e0826e082f784f30a758b7996f3bfab8bc0a5f23
lafeingcrokodil <tvhtoo@gmail.com>
2025-08-02 11:44:54 +0200

    Split Google project setup to separate file

---
+++ infra/google.tf
@@ -0,0 +1,27 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
+}
+
+resource "google_project" "project" {
+  name            = var.project_name
+  project_id      = var.project_id
+  billing_account = var.billing_account_id
+
+  auto_create_network = false
+}
+
+resource "google_project_service" "enable_apis" {
+  for_each = toset([
+    "artifactregistry.googleapis.com",
+    "compute.googleapis.com",
+    "firebaserules.googleapis.com",
+    "firestore.googleapis.com",
+    "iamcredentials.googleapis.com",
+    "oslogin.googleapis.com",
+    "run.googleapis.com",
+    "sheets.googleapis.com",
+  ])
+  service = each.key
+}
--- infra/main.tf
+++ infra/main.tf
@@ -11,34 +11,6 @@ terraform {
   }
 }
 
-provider "google" {
-  project = var.project_id
-  region  = var.region
-  zone    = var.zone
-}
-
-resource "google_project" "project" {
-  name            = var.project_name
-  project_id      = var.project_id
-  billing_account = var.billing_account_id
-
-  auto_create_network = false
-}
-
-resource "google_project_service" "enable_apis" {
-  for_each = toset([
-    "artifactregistry.googleapis.com",
-    "compute.googleapis.com",
-    "firebaserules.googleapis.com",
-    "firestore.googleapis.com",
-    "iamcredentials.googleapis.com",
-    "oslogin.googleapis.com",
-    "run.googleapis.com",
-    "sheets.googleapis.com",
-  ])
-  service = each.key
-}
-
 resource "google_storage_bucket" "terraform_state" {
   name          = var.terraform_bucket
   location      = var.region
